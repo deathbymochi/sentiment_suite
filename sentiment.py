d66c075ce65f27b0f2c1d2520b0adc69e1358d41
Alex Sippel <asippel89@gmail.com>
2013-09-22 10:43:26 -0500

    forgot sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -4,16 +4,12 @@ import re
 import os
 
 class SentimentFactory(object):
-	"""Factory class for creating instances of 
-	library runs
-	"""
-
+	"""Factory class for creating instances of library runs"""
 	def __init__(self):
 		pass
 
 class LibraryRun(object):
 	"""Class that runs library on given input text"""
-
 	def __init__(self):
 		pass
 
