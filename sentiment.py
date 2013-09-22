9e2ca52fbf8bb7b01dc5e5f1f07e0478323fad05
Alex Sippel <asippel89@gmail.com>
2013-09-22 12:59:22 -0500

    started work on SentimentFactory structure, some tests for clean_row

--- sentiment.py
+++ sentiment.py
@@ -3,10 +3,48 @@
 import re
 import os
 
+def clean_row(row):
+	"""Cleans row from text file, outputs tuple (ID, cleaned_text)"""
+	text_id, text = row.split('\t')
+	text = text.lower()
+	text = re.sub(r'[^\w\s]', '', text)
+	return (text_id, text)
+
 class SentimentFactory(object):
 	"""Factory class for creating instances of library runs"""
-	def __init__(self):
+	def __init__(self, text_filepath, library_filepath, output_directory=None, 
+		        output_filename=None):
+		self.text_filepath = text_filepath
+		self.library_filepath = library_filepath
+		self.output_directory = output_directory
+		self.output_filename = output_filename
+
+	def run_suite(self):
+		"""Starts library runs for each essay"""
+		library = get_library_from_file(self.library_filepath)
+
+		texts = get_next_text_from_file(self.text_filepath)
+		for text in texts:
+			append_to_output_file(LibraryRun(essay, library))
+
+	def get_library_from_file(self, library_filepath):
+		"""Load library from filepath"""
 		pass
+
+	def get_next_text_from_file(self, text_filepath):
+		"""Stream next line from text file"""
+		with open(text_filepath, "r") as full_text:
+			next_clean_line = clean_row(full_text.readline())
+			pass
+
+
+
+
+	def append_to_output_file(self, output_line, output_filepath):
+		"""Appends single line to an output file"""
+		pass
+
+
 
 class LibraryRun(object):
 	"""Class that runs library on given input text"""
--- sentiment_tests.py
+++ sentiment_tests.py
@@ -3,18 +3,29 @@
 import unittest
 import sentiment
 
+class TestCleanRowFunction(unittest.TestCase):
+	"""Tests clean_row function cleans rows properly"""
+	def test_clean_row_punctuation(self):
+		"""Tests clean_row removes punctuation correctly"""
+		obj_ut = sentiment.clean_row(
+			'100\tan "apple...:" is it yellow-green, or red/orange?')
+		self.assertEqual(obj_ut[1], "an apple is it yellowgreen or redorange")
+		
 class TestSentimentFactory(unittest.TestCase):
 	"""Tests for the SentimentFactory class"""
 	def test_instantiate_sentiment_factory(self):
 		"""Tests we can instantiate SentimentFactory"""
-		obj_ut = sentiment.SentimentFactory()
+		obj_ut = sentiment.SentimentFactory("", "")
 		self.assertIsInstance(obj_ut, sentiment.SentimentFactory)
+
+
 
 class TestLibraryRun(unittest.TestCase):
 	"""Tests for the LibraryRun class"""
 	def test_instantiate_library_run(self):
 		obj_ut = sentiment.LibraryRun()
 		self.assertIsInstance(obj_ut, sentiment.LibraryRun)
+
 
 if __name__ == '__main__':
 	unittest.main()
